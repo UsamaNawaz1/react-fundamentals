202c9bc5bdbf031c48d783daffa390dcf1b1c7bb
dreh-web <usamanawaz1994@gmail.com>
2021-08-03 23:55:21 +0500

    first commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import reactDom from "react-dom";
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section>
+      <Book />
+    </section>
   );
 }
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+
+const Image = () => (
+  <img src='https://images-na.ssl-images-amazon.com/images/I/41wBo8-XN7L._AC_SX368_.jpg' />
+);
+
+const Title = () => <h1>The Official SAT Guide Book</h1>;
+
+const Author = () => <h4>Ali Usama Nawaz</h4>;
 
 // const Greeting = () => {
 //   return React.createElement(
@@ -17,4 +35,4 @@ function Greeting() {
 //   );
 // };
 
-reactDom.render(<Greeting />, document.getElementById("root"));
+reactDom.render(<BookList />, document.getElementById("root"));
