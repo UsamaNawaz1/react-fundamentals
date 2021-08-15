7e112cf3424bb2bbf2c679062e01f5d341496e9c
dreh-web <usamanawaz1994@gmail.com>
2021-08-15 15:56:24 +0500

    Adding new changes

---
+++ my-app/src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41wBo8-XN7L._AC_SX368_.jpg",
+    title: "New SAT Book",
+    author: "Ali Usama Nawaz",
+  },
+
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41BzGc2MBYS._SX329_BO1,204,203,200_.jpg",
+    title: "Blind Tiger",
+    author: "Sandra Brown",
+  },
+];
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,21 +2,7 @@ import React from "react";
 import reactDom from "react-dom";
 //css
 import "./index.css";
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/41wBo8-XN7L._AC_SX368_.jpg",
-    title: "New SAT Book",
-    author: "Ali Usama Nawaz",
-  },
-
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/41BzGc2MBYS._SX329_BO1,204,203,200_.jpg",
-    title: "Blind Tiger",
-    author: "Sandra Brown",
-  },
-];
+import { books } from "./books";
 
 function BookList() {
   return (
