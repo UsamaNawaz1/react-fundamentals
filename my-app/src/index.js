c98a7d0e3b160740aa21d185986e5da8f1aba749
dreh-web <usamanawaz1994@gmail.com>
2021-08-15 14:25:33 +0500

    Adding new changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,48 +2,31 @@ import React from "react";
 import reactDom from "react-dom";
 //css
 import "./index.css";
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/41wBo8-XN7L._AC_SX368_.jpg",
-  title: "New SAT Book",
-  author: "Ali Usama Nawaz",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41wBo8-XN7L._AC_SX368_.jpg",
+    title: "New SAT Book",
+    author: "Ali Usama Nawaz",
+  },
 
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/41BzGc2MBYS._SX329_BO1,204,203,200_.jpg",
-  title: "Blind Tiger",
-  author: "Sandra Brown",
-};
-
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41BzGc2MBYS._SX329_BO1,204,203,200_.jpg",
+    title: "Blind Tiger",
+    author: "Sandra Brown",
+  },
+];
+const names = ["ali", "usama", "nawaz"];
+const newName = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newName);
 function BookList() {
-  return (
-    <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>lorem20 adnasd jkasdjkashdjbdjkasbfsbjksdkfbsjkdfbskdb j</p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className='booklist'>{newName}</section>;
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
   // const { img, title, author } = props;
-  return (
-    <article className='book'>
-      <img src={img} />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      {children}
-    </article>
-  );
+  return <article className='book'></article>;
 };
 
 // const Greeting = () => {
