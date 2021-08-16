f8ce20f7705483f2f6439aafe226a7ba20a5bf55
dreh-web <usamanawaz1994@gmail.com>
2021-08-17 00:19:42 +0500

    Adding new changes

--- my-app/src/tutorial/1-useState/setup/4-useState-object.js
+++ my-app/src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,14 +6,20 @@ const UseStateObject = () => {
     age: 24,
     message: "random message",
   });
+
+  const [name, setName] = useState("peter");
+  const [age, setage] = useState(24);
+  const [message, setmessage] = useState("Random Message");
+
   const changeMessage = () => {
-    setPeople({ ...people, message: "hello world" });
+    // setPeople({ ...setPeople, message: "hello world" });
+    setmessage("hello world");
   };
   return (
     <>
-      <h3>{people.name}</h3>
-      <h3>{people.age}</h3>
-      <h3>{people.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className='btn' onClick={changeMessage}>
         change message
       </button>
