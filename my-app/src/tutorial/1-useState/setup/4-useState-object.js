6fa0efafcc1f70d935ad50f5f60139f152b314ba
dreh-web <usamanawaz1994@gmail.com>
2021-08-17 00:07:25 +0500

    Adding new changes

--- my-app/src/tutorial/1-useState/setup/4-useState-object.js
+++ my-app/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [people, setPeople] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+  const changeMessage = () => {
+    setPeople({ ...people, message: "hello world" });
+  };
+  return (
+    <>
+      <h3>{people.name}</h3>
+      <h3>{people.age}</h3>
+      <h3>{people.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
