cb8c87502e784fd0f91b339c8c1e6d1b8cf84248
dreh-web <usamanawaz1994@gmail.com>
2021-08-18 01:31:35 +0500

    Adding new changes

--- my-app/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ my-app/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -6,6 +6,11 @@ const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   useEffect(() => {
     console.log("call useEffect");
+    if (value > 0) {
+      document.title = `New Messages (${value})`;
+    } else {
+      document.title = "React App";
+    }
   });
   console.log("render component");
   return (
