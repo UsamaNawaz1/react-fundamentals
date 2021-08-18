8b806a22942abe08b1b7a60c1de256db1e724623
dreh-web <usamanawaz1994@gmail.com>
2021-08-19 00:09:23 +0500

    Adding new changes

--- my-app/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ my-app/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,12 +4,17 @@ import React, { useState } from 'react'
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
   const firstValue = text || 'hello world'
   const secondValue = text && 'hello world'
 
   return (
     <>
       <h1>{text || 'John Doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
     </>
   )
 }
