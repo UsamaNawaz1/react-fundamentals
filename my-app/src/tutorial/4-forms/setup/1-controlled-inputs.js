86c1b614068c0e997044d8b18b608c43c8de43fb
dreh-web <usamanawaz1994@gmail.com>
2021-08-20 01:00:31 +0500

    Adding new changes

--- my-app/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ my-app/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,24 +6,36 @@ import React, { useState } from 'react'
 // value, onChange
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState('');
+  const [email, Email] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world');
+  };
   return (
     <>
       <article>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
           <div className='form-control'>
             <label htmlFor='firstName'>Name: </label>
-            <input type='text' id='firstName' name='firstName' />
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
 
           <div className='form-control'>
             <label htmlFor='email'>Email: </label>
-            <input type='text' id='email' name='email' />
+            <input type='text' id='email' name='email' value={email} />
           </div>
           <button type='submit'>add person</button>
         </form>
       </article>
     </>
-  )
-}
+  );
+};
 
-export default ControlledInputs
+export default ControlledInputs;
