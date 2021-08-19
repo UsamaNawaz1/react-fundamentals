6462684dad9ef27b7cacc45b8c5ed2b3c37dd391
dreh-web <usamanawaz1994@gmail.com>
2021-08-19 19:55:33 +0500

    Adding new changes

--- projects/03-reviews/setup/src/Review.js
+++ projects/03-reviews/setup/src/Review.js
@@ -5,6 +5,26 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 const Review = () => {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number
+  }
+  const nextPerson = () => {
+    setIndex((index) => {
+      return checkNumber(index + 1)
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      return checkNumber(index - 1)
+    })
+  }
   return (
     <article className='review'>
       <div className='img-container'>
@@ -17,10 +37,10 @@ const Review = () => {
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
       <div className='button-container'>
-        <button className='next-btn'>
+        <button className='next-btn' onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className='prev-btn'>
+        <button className='prev-btn' onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
