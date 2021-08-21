1a5db5b50b91f755744a59917abacc5bb703ac8d
dreh-web <usamanawaz1994@gmail.com>
2021-08-21 23:27:28 +0500

    adding new project

--- projects/10-grocery-bud/setup/src/App.js
+++ projects/10-grocery-bud/setup/src/App.js
@@ -8,7 +8,26 @@ function App() {
   const [isEditing, setIsEdition] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
-  return <h2>grocery bud setup</h2>;
+  const handleSubmit = (e) => {
+    e.preventDeafult();
+    console.log('hello');
+  };
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input type='text' />
+          <button type='submit' className='submit-btn'></button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
