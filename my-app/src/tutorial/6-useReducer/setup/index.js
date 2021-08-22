31de63ce08e2c24e84b6e49b284766719d1464bd
dreh-web <usamanawaz1994@gmail.com>
2021-08-22 16:41:41 +0500

    Adding new changes

--- my-app/src/tutorial/6-useReducer/setup/index.js
+++ my-app/src/tutorial/6-useReducer/setup/index.js
@@ -3,23 +3,24 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 
+const reducer = (state, action) => {};
+
+const defaultState = {
+  people: data,
+  isModelOpen: false,
+  modelContent: 'hello',
+};
 const Index = () => {
-  const [people, setPeople] = useState(data);
-  const [showModel, setShowModel] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
   const [name, setName] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModel(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name }]);
-      setName('');
-    } else {
-      setShowModel(true);
     }
   };
   return (
     <>
-      {showModel && <Modal />}
+      {state.isModelOpen && <Modal modelContent={state.modelContent} />}
       <form onSubmit={handleSubmit} className='form'>
         <div>
           <input
@@ -30,7 +31,7 @@ const Index = () => {
         </div>
         <button type='submit'>Add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div>
